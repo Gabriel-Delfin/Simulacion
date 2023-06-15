caa38d08923c5058f11c3f724a917bf060b4433b
Edgar Ramirez <leramirezp@hotmail.com>
2023-06-15 01:22:29 -0500

    .

--- Assets/Scripts/FetchDataFromAPI.cs
+++ Assets/Scripts/FetchDataFromAPI.cs
@@ -8,19 +8,32 @@ using Newtonsoft.Json;
 public class AgentData
 {
     public string id;
-    public int[] pos;
+    public int posX;
+    public int posY;
     public string type;
     public bool display;
 }
 
+
 public class FetchDataFromAPI : MonoBehaviour
 {
-    public GameObject prefabRescuerAgent;
-    public GameObject prefabVictimAgent;
+    public Transform sceneRoot;
+    public GameObject prefabAgent;
 
     private string apiUrl = "http://127.0.0.1:8585/step";
+    private Dictionary<string, GameObject> agentsMap = new Dictionary<string, GameObject>();
 
-    void Start()
+    public void OnEnable()
+    {
+        Timer.OnMinuteChanged += Run;
+    }
+
+    public void OnDisable()
+    {
+        Timer.OnMinuteChanged -= Run;
+    }
+
+    void Run()
     {
         StartCoroutine(FetchDataFromPython());
     }
@@ -41,7 +54,33 @@ public class FetchDataFromAPI : MonoBehaviour
             
             // Parse the JSON response and store it in a C# HashMap
             AgentData[] data = JsonConvert.DeserializeObject<AgentData[]>(jsonResponse);
-            
+
+            // Iterate through the data array
+            foreach (var agent in data)
+            {
+                string id = agent.id;
+                GameObject agentObj;
+
+                // Check if a GameObject with a Agent component exists for the current id
+                if (!agentsMap.ContainsKey(id))
+                {
+                    // Instantiate a new Agent prefab
+                    agentObj = Instantiate(prefabAgent, sceneRoot);
+
+                    // Add the new Agent to the dictionary
+                    agentsMap.Add(id, agentObj);
+                }
+                else {
+                    agentsMap.TryGetValue(id, out agentObj);
+                }
+                
+                // Set the id for the new Agent
+                agentObj.GetComponent<PersonAgent>().id = id;
+                agentObj.GetComponent<PersonAgent>().type = agent.type;
+                agentObj.GetComponent<PersonAgent>().positionToBe = new int[] { agent.posX, agent.posY };
+
+                // The updates to the new position will be done individually in each agent
+            }
         }
     }
 }
---
+++ Assets/Scripts/PersonAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAgent : MonoBehaviour
+{
+    public string id;
+    public string type;
+    public int[] positionToBe = new int[] { 0, 0 }; 
+
+    void Update () {
+        // Move to the given position
+    }
+}
