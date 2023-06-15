de7dc1336cddfef84865c5720f01ad389d58faeb
Gabriel-Delfin <100104960+Gabriel-Delfin@users.noreply.github.com>
2023-06-14 19:54:16 -0600

    SafeZone funcional
    victima desaparece en safezone

---
+++ Assets/Scripts/ReturnOrigin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnOrigin : MonoBehaviour
+{
+    [SerializeField] GameObject _victim;
+    public Vector3 safeZone;
+    public Vector3 safeZone2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x == safeZone.x && transform.position.y == safeZone.y && transform.position.z == safeZone.z)
+        {
+            _victim.SetActive(false);
+        }
+        else if(transform.position.x == safeZone2.x && transform.position.y == safeZone2.y && transform.position.z == safeZone2.z)
+        {
+            _victim.SetActive(false);
+        }
+    }
+}
