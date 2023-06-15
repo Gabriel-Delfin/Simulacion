3b901e97b5ddec9b20870f93b4a0107077267629
Edgar Ramirez <leramirezp@hotmail.com>
2023-06-15 01:54:37 -0500

    .

--- Assets/Scripts/PersonAgent.cs
+++ Assets/Scripts/PersonAgent.cs
@@ -7,8 +7,51 @@ public class PersonAgent : MonoBehaviour
     public string id;
     public string type;
     public int[] positionToBe = new int[] { 0, 0 }; 
+    public Sprite victimSprite;
 
-    void Update () {
-        // Move to the given position
+    public float moveSpeed = 5f;
+    public LayerMask whatStopsMovement;
+
+    void Start()
+    {
+        if (type == "victim")
+        {
+            // Get the SpriteRenderer component attached to the current GameObject
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer != null)
+            {
+                // Change the sprite to the newSprite
+                spriteRenderer.sprite = victimSprite;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(positionToBe[0], positionToBe[1], 0);
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+
+        // if (Vector3.Distance (transform.position, newPosition) <= .05f)
+        // {
+        //     if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        //     {
+        //         if (!Physics2D.OverlapCircle(newPosition + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+        //         {
+        //             newPosition += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        //         }
+        //     }
+
+        //     if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+        //     {
+        //         if (!Physics2D.OverlapCircle(newPosition + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+        //         {
+        //             newPosition += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        //         }
+        //     }
+        // }
+
+        
     }
 }
